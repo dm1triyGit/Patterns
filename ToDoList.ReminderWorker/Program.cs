c925c60c810c533afcd209a7982961877f7b9ab5
Dmitriy Lipilin <LipilinD@quantumart.ru>
2023-07-14 12:08:55 +0300

    init worker

---
+++ ToDoList.ReminderWorker/Program.cs
@@ -0,0 +1,10 @@
+using ToDoList.ReminderWorker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
